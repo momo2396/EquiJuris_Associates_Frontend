2147eaa498c5aa1aa3df0e4d881aa78601bf140f
momo2396 <foyazunnesamomo.345@gmail.com>
2023-08-05 00:23:47 +0600

    placeholder color updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import { BsFacebook } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
 function App() {
   const [count, setCount] = useState(0);
-  // console.log(lawyerImage);
   return (
     <>
       <header>
@@ -59,7 +58,9 @@ function App() {
                 </li>
               </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">EquiJuris Associates</a>
+            <a className="btn btn-ghost normal-case text-xl">
+              EquiJuris Associates
+            </a>
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
@@ -322,7 +323,7 @@ function App() {
               <div className="relative bg-base-100 shadow-xl pb-3">
                 <img src={lawyerImage} alt="lawyer" />
               </div>
-              <div className="p-3 -ml-3 -mt-16 relative z-50 flex justify-center bg-base-100 rounded-lg shadow-xl w-[200px]">
+              <div className="p-3 -ml-3 -mt-16 relative z-10 flex justify-center bg-base-100 rounded-lg shadow-xl w-[200px]">
                 <div>
                   <div className="flex justify-between">
                     <button className="btn bg-transparent border-none">
@@ -345,7 +346,7 @@ function App() {
               <div className="relative bg-base-100 shadow-xl pb-3">
                 <img src={lawyerImage} alt="lawyer" />
               </div>
-              <div className="p-3 -ml-3 -mt-16 relative z-50 flex justify-center bg-base-100 rounded-lg shadow-xl w-[200px]">
+              <div className="p-3 -ml-3 -mt-16 relative z-10 flex justify-center bg-base-100 rounded-lg shadow-xl w-[200px]">
                 <div>
                   <div className="flex justify-between">
                     <button className="btn bg-transparent border-none">
@@ -368,7 +369,7 @@ function App() {
               <div className="relative bg-base-100 shadow-xl pb-3">
                 <img src={lawyerImage} alt="lawyer" />
               </div>
-              <div className="p-3 -ml-3 -mt-16 relative z-50 flex justify-center bg-base-100 rounded-lg shadow-xl w-[200px]">
+              <div className="p-3 -ml-3 -mt-16 relative z-10 flex justify-center bg-base-100 rounded-lg shadow-xl w-[200px]">
                 <div>
                   <div className="flex justify-between">
                     <button className="btn bg-transparent border-none">
@@ -390,33 +391,50 @@ function App() {
           </div>
         </section>
         {/* appointment section */}
-        <div class="flex justify-center  items-center w-screen m-auto bg-[#1f2732]">
+        <section>
+          <div class="flex justify-center  items-center w-screen m-auto appointment_background">
             <div class="container w-full m-auto my-4 px-4 lg:px-20">
-
               <div class="w-full p-8 my-4 md:px-12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl bg-[#141516]">
                 <div class="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-                  <input class="w-full bg-[#141516] text-white mt-2 p-3 border-b-[1px] placeholder-white border-slate-400 focus:outline-none focus:shadow-outline"
-                      type="text" placeholder="Full Name*" />
-                  <input class="w-full bg-[#141516] text-white-900 mt-2 p-3 border-b-[1px] placeholder-white  border-slate-400 focus:outline-none focus:shadow-outline"
-                      type="email" placeholder="Email Address*" />
-                  <input class="w-full bg-[#141516] text-white mt-2 p-3 border-b-[1px] placeholder-white border-slate-400 focus:outline-none focus:shadow-outline"
-                      type="text" placeholder="Phone Number*" />
-                  <input class="w-full bg-[#141516] text-white mt-2 p-3 border-b-[1px] placeholder-white border-slate-400 focus:outline-none focus:shadow-outline"
-                      type="text" placeholder="Subject*" />
-                  </div>
-                  <div class="my-4">
-                    <textarea placeholder="Your Message*" class="w-full h-32 bg-[#141516] placeholder-white text-white mt-2 p-3 border-b-[1px] border-slate-400 focus:outline-none focus:shadow-outline"></textarea>
-                  </div>
-                  <div class="my-2  flex items-center justify-center">
-                    <button class="btn capitalize text-sm font-bold tracking-wide bg-white text-black p-3 rounded-lg 
-                                focus:outline-none focus:shadow-outline">
-                      Get An Appointment
-                    </button>
-                  </div>
-			          </div>
+                  <input
+                    class="w-full bg-[#141516] text-white mt-2 p-3 border-b-[1px] placeholder-gray-400 border-slate-400 focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Full Name*"
+                  />
+                  <input
+                    class="w-full bg-[#141516] text-white-900 mt-2 p-3 border-b-[1px] placeholder-gray-400  border-slate-400 focus:outline-none focus:shadow-outline"
+                    type="email"
+                    placeholder="Email Address*"
+                  />
+                  <input
+                    class="w-full bg-[#141516] text-white mt-2 p-3 border-b-[1px] placeholder-gray-400 border-slate-400 focus:outline-none focus:shadow-outline "
+                    type="text"
+                    placeholder="Phone Number*"
+                  />
+                  <input
+                    class="w-full bg-[#141516] text-white mt-2 p-3 border-b-[1px] placeholder-gray-400 border-slate-400 focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Subject*"
+                  />
+                </div>
+                <div class="my-4">
+                  <textarea
+                    placeholder="Your Message*"
+                    class="w-full h-32 bg-[#141516] placeholder-gray-400 text-white mt-2 p-3 border-b-[1px] border-slate-400 focus:outline-none focus:shadow-outline"
+                  ></textarea>
+                </div>
+                <div class="my-2  flex items-center justify-center">
+                  <button
+                    class="btn capitalize text-sm font-bold tracking-wide bg-white text-black p-3 rounded-lg 
+                                focus:outline-none focus:shadow-outline"
+                  >
+                    Get An Appointment
+                  </button>
+                </div>
               </div>
+            </div>
           </div>
-
+        </section>
 
         {/* blog card: m */}
         <section className="bg-[#1F2732] py-20 text-white">
@@ -571,24 +589,59 @@ function App() {
         </section>
       </main>
       <footer class="footer footer-center p-10 bg-[#1F2732] text-white opacity-90">
-        <div><h2 class="text-3xl font-extrabold">EquiJuris Associates</h2></div>
+        <div>
+          <h2 class="text-3xl font-extrabold">EquiJuris Associates</h2>
+        </div>
         <div class="grid grid-flow-col gap-4">
-          <a class="link link-hover hover:no-underline">Home</a> 
-          <a class="link link-hover hover:no-underline">About</a> 
-          <a class="link link-hover hover:no-underline">Contact</a> 
+          <a class="link link-hover hover:no-underline">Home</a>
+          <a class="link link-hover hover:no-underline">About</a>
+          <a class="link link-hover hover:no-underline">Contact</a>
           <a class="link link-hover hover:no-underline">Blog</a>
           <a class="link link-hover hover:no-underline">Lawyers</a>
           <a class="link link-hover hover:no-underline">Practice Areas</a>
-        </div> 
+        </div>
         <div>
           <div class="grid grid-flow-col gap-4">
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                class="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                class="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                class="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
           </div>
-        </div> 
+        </div>
         <div>
-          <p>Copyright © 2023 - All right reserved by EquiJuris Associates Limited</p>
+          <p>
+            Copyright © 2023 - All right reserved by EquiJuris Associates
+            Limited
+          </p>
         </div>
       </footer>
     </>
