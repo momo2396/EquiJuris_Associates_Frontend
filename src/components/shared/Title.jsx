406dc3863d4011526c9f2997458de5b8cff72479
momo2396 <foyazunnesamomo.345@gmail.com>
2023-12-27 15:41:08 +0600

    about us, contact us page positioning, color, text, background image updated

--- src/components/shared/Title.jsx
+++ src/components/shared/Title.jsx
@@ -1,8 +1,13 @@
+import Icon1 from "../../assets/customicon/icon1";
+
 const Title = ({ heading, subHeading }) => {
   return (
     <div className="text-white text-center font-titleFont">
       <p className="text-4xl font-thin py-3">{heading}</p>
-      <div className="border border-[#D1B06B]"></div>
+      {/* <div className="border border-[#D1B06B]"></div> */}
+      <div className="flex justify-center items-center">
+        <Icon1></Icon1>
+      </div>
       <p className=" text-base font-thin pt-3 text-center">{subHeading}</p>
     </div>
   );
